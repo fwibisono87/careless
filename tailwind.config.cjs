1349043eefe1c400d1900704f0ef39fc969c6e19
Francis Wibisono <code@franciswibisono.com>
2023-01-01 20:50:24 +0700

    feat: kickoff revamp

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,22 +1,8 @@
-const config = {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
-
-	theme: {
-		extend: {
-			fontFamily: {
-				poppins: ['Poppins', 'sans-serif'],
-				lora: ['Lora', 'serif'],
-				dancing: ['Dancing Script', 'cursive']
-			},
-			colors: {
-				care: '#E093E5',
-				darkCare: '#605ca8',
-				grayCare: '#333333'
-			}
-		}
-	},
-
-	plugins: []
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
 };
-
-module.exports = config;
