0c59d90d5e9ca4d994ba1eba35c0e11c70fe0604
Francis Wibisono <code@franciswibisono.com>
2023-01-01 21:49:22 +0700

    feat: init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,39 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { writable } from 'svelte/store';
+	import { fade, blur, fly, slide, scale } from 'svelte/transition';
+
+	export const yScroll = writable(0);
+	let y = 0;
+    const scrollThreshold = 10
+
+	$: yScroll.set(y);
+
+	const heroImage0 = 'https://is3.cloudhost.id/francis/public/careless/image/careless_profile.png';
+
+	const heroImage1 =
+		'https://is3.cloudhost.id/francis/public/careless/image/cropped/Fullbody_0_testdelete_seperate1(1).png';
+</script>
+
+<div class="flex flex-col">
+	<div
+		class="flex flex-col gap-4 justify-between {$yScroll < scrollThreshold ? 'min-h-[65vh]' : 'min-h-screen'}"
+	>
+		<div class="flex flex-col gap-2 top-2 sticky">
+			<h1 class="font-bold text-2xl lg:text-5xl text-greenc text-center">
+				CarelessTheStray<span class="text-base text-lilac">.com</span>
+			</h1>
+			<span class="text-xl lg:text-3xl font-bold text-lilac text-center font-courgette"
+				>Home of your favourite stray!</span
+			>
+		</div>
+		<div>
+			{#if $yScroll < scrollThreshold}
+				<img src={heroImage0} alt="Careless The Stray" class="w-64 max-h-64 mx-auto" in:fade/>
+			{:else}
+				<img src={heroImage1} alt="Careless The Stray" class="w-64  mx-auto mt-32" in:fade/>{/if}
+			<div class="bg-lilac text-seashell text-center">scroll down {$yScroll}</div>
+		</div>
+	</div>
+</div>
+
+<svelte:window bind:scrollY={y} />
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {}
-  },
-  plugins: []
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				poppins: ['Poppins', 'sans-serif'],
+				lora: ['Lora', 'serif'],
+				dancing: ['Dancing Script', 'cursive'],
+        courgette: ['Courgette', 'cursive']
+			},
+			colors: {
+				onyx: '#444444',
+				greenc: '#c4df9b',
+				ruby: '#a30f14',
+				lilac: '#9860a2',
+				seashell: '#fdf0eb'
+			}
+		}
+	},
+	plugins: []
 };
