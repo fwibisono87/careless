51944d3fe9f79a080e3722f973cc8f4d7a592fb2
Francis Wibisono <code@franciswibisono.com>
2023-01-02 09:53:05 +0700

    fix: more animation fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,27 +18,44 @@
 
 <div class="flex flex-col">
 	<div class="flex flex-col gap-4 justify-between min-h-[75vh]">
-		<div class="flex flex-col gap-2 top-0 sticky bg-onyx z-50 py-8">
+		<div
+			class="flex flex-col gap-2 top-0 sticky bg-onyx z-50 py-4 lg:py-8 {y > scrollThreshold
+				? 'shadow-lg'
+				: ''}"
+		>
 			<h1 class="font-bold text-2xl lg:text-5xl text-greenc text-center">
 				CarelessTheStray<span class="text-base text-lilac">.com</span>
 			</h1>
-			<span class="text-xl lg:text-3xl font-bold text-lilac text-center font-courgette"
-				>Home of your favourite stray!</span
-			>
+			{#if y < scrollThreshold}
+				<span class="text-xl lg:text-3xl font-bold text-lilac text-center font-courgette"
+					>Home of your favourite stray!</span
+				>
+			{/if}
 		</div>
 		<div class="transition-all">
 			<img
 				src={heroImage0}
 				alt="Careless The Stray"
-				class="w-64 max-h-64 mx-auto object-cover z-0 transition-all {y < scrollThreshold ? '-mb-32' : 'mb-0'}"
+				class="w-64 max-h-64 mx-auto object-cover z-0 drop-shadow transition-all {y <
+				scrollThreshold
+					? '-mb-32'
+					: 'mb-0'}"
 				in:fade
 			/>
-			<div
-				class="bg-lilac text-seashell text-center h-32 z-20 relative flex flex-col"
-			>
-				<MaterialSymbolsKeyboardArrowDown class="h-16 w-16 mx-auto mt-2 animate-pulse" />
-				{y}
-				<div class="w-full h-32 bg-onyx" />
+			<div class="bg-lilac text-seashell text-center min-h-32 z-20 relative flex flex-col">
+				{#if y < scrollThreshold}
+					<MaterialSymbolsKeyboardArrowDown class="h-16 w-16 mx-auto mt-2 animate-pulse" />
+				{:else}
+					<p class="mx-auto w-[80%] lg:w-[35%] font-semibold py-4">
+						"Welcome and heya! I'm careless the stray virtual person. Anyway if you got some time
+						come join me as I stumble through a whole bunch of stuff, while trying to be
+						entertaining. If you can stand the cringe and technical difficulties then welcome
+						aboard!"
+					</p>
+				{/if}
+				{#if y < scrollThreshold}
+					<div class="w-full h-32 bg-onyx" />
+				{/if}
 			</div>
 		</div>
 	</div>
